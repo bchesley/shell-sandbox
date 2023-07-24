873dee0f12e48b6f6847b8ea2c59e2c07bc52d4a
Bob Chesley <bob@bobchesley.net>
2023-07-24 11:18:30 -0400

    Enhance to allow passing '.' as the first parameter in which case the script will search all subdirectories in the current working directory for the provided string or strings.

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # shell-sandbox
 Shell scripts of interest
+
+Links:
+- [Shell Scripting Tutorial](https://www.shellscript.sh/)
+- https://github.com/alebcay/awesome-shell - A curated list of awesome command-line frameworks, toolkits, guides and gizmos. Inspired by awesome-php.
+- 
--- src/search_directories.sh
+++ src/search_directories.sh
@@ -1,28 +1,45 @@
 #!/bin/bash
 
+# Function to search in a specific subdirectory
+search_subdirectory() {
+  local subdirectory="$1"
+  shift
+
+  echo "Searching in subdirectory '$subdirectory':"
+
+  for search_string in "$@"; do
+    echo "  Searching for '$search_string':"
+
+    # Use ripgrep with the provided flags to search for the string in the subdirectory
+      # The "-l" flag tells ripgrep to only output the names of files that have a match
+      # The "-r" flag enables recursive searching
+      # The "-F" flag treats the search string as a literal string
+      # The "--color=always" flag enables colored output
+      # The "--no-messages" flag suppresses error messages from being displayed
+      # The "--hidden" flag searches hidden files and directories
+      # The "--ignore-case" flag makes the search case-insensitive
+      # You can customize these flags as per your requirements.
+    rg -lFr --color=always --no-messages --hidden --ignore-case "$search_string" "$subdirectory"
+    echo
+  done
+}
+
 # Check if the correct number of arguments is provided
-if [ "$#" -lt 2 ]; then
+if [ "$#" -lt 1 ]; then
   echo "Usage: $0 subdirectory search_strings..."
   exit 1
 fi
 
-# Get the subdirectory path from the first argument
-subdirectory="$1"
-shift
+# Check if the first argument is "."
+if [ "$1" = "." ]; then
+  # Loop through all subdirectories in the current directory
+  for subdirectory in */; do
+    [ -d "$subdirectory" ] || continue  # Skip non-directories
 
-# Loop over the remaining arguments (search strings)
-for search_string in "$@"; do
-  echo "Searching for '$search_string' in subdirectory '$subdirectory':"
-
-  # Use ripgrep to search for the string in the subdirectory
-  # The "-l" flag tells ripgrep to only output the names of files that have a match
-  # The "-r" flag enables recursive searching
-  # The "-F" flag treats the search string as a literal string
-  # The "--color=always" flag enables colored output
-  # The "--no-messages" flag suppresses error messages from being displayed
-  # The "--hidden" flag searches hidden files and directories
-  # The "--ignore-case" flag makes the search case-insensitive
-  # You can customize these flags as per your requirements.
-  rg -lFr --color=always --no-messages --hidden --ignore-case "$search_string" "$subdirectory"
-  echo
-done
+    # Apply the search strings to each subdirectory
+    search_subdirectory "$subdirectory" "${@:2}"
+  done
+else
+  # If the first argument is not ".", treat it as a specific subdirectory
+  search_subdirectory "$@"
+fi
